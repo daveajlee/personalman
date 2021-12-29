338d476e7919aada10812623a576db01dcaa5ee5
Dave Lee <dave@davelee.de>
2021-12-29 21:40:14 +0100

    Add Eureka and environment variable for Mongo DB

--- server/src/main/java/de/davelee/personalman/server/PersonalManServerApplication.java
+++ server/src/main/java/de/davelee/personalman/server/PersonalManServerApplication.java
@@ -1,8 +1,12 @@
 package de.davelee.personalman.server;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Lazy;
 
 /**
  * This class represents the Spring Boot Application for the PersonalMan Server Application.
@@ -12,6 +16,13 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"de.davelee.personalman.server.rest.controllers","de.davelee.personalman.server.configuration",
 "de.davelee.personalman.server.services"})
 public class PersonalManServerApplication {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
 
     public static void main ( String[] args ) {
         SpringApplication.run(PersonalManServerApplication.class, args);
