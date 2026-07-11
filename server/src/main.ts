a4f8d38289f90ad57e96b5c2bd586600eeae30b4
David A J Lee <dave@davelee.de>
2026-07-11 14:36:21 +0200

    Enable CORS for React Client running on same interface for test purposes

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('PersonalMan')
